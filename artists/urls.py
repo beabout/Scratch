026c181e4e9f17e875b9598e6d5f464583971507
beabout <beaboutclayton@gmail.com>
2023-04-16 13:56:42 -0500

    artists: add album view

--- artists/urls.py
+++ artists/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:artist_id>/", views.detail, name="detail"),
+    path("<int:artist_id>/albums/<int:album_id>", views.album, name="album"),
 ]
--- artists/views.py
+++ artists/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from . models import Artist
+from albums.models import Album
 
 def index(request):
     template = loader.get_template("artists/index.html")
@@ -16,5 +17,14 @@ def detail(request, artist_id):
   context = { 
       'artist': artist,
       'albums': artist.album_set.all(),
+  }
+  return HttpResponse(template.render(context, request))
+
+def album(request, artist_id, album_id):
+  template = loader.get_template("albums/detail.html")
+  album = Album.objects.get(id=album_id)
+  context = { 
+      'album': album,
+      'artists': album.artists.all(),
   }
   return HttpResponse(template.render(context, request))
