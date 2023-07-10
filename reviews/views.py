e258d0fa83257cb75009c6638fd2a0b9f2b6dcdb
beabout <beaboutclayton@gmail.com>
2023-07-10 13:02:57 -0500

    add reviews/new route

--- reviews/views.py
+++ reviews/views.py
@@ -6,11 +6,11 @@ from . models import Review, Section
 from albums.models import Album
 
 def index(request):
-    template = loader.get_template("reviews/index.html")
-    context = { 
-        "reviews": Review.objects.all(),
-    }
-    return HttpResponse(template.render(context, request))
+  template = loader.get_template("reviews/index.html")
+  context = { 
+      "reviews": Review.objects.all(),
+  }
+  return HttpResponse(template.render(context, request))
 
 def detail(request, review_id):
   template = loader.get_template("reviews/detail.html")
@@ -21,3 +21,13 @@ def detail(request, review_id):
       "sections": Section.objects.filter(review_id=review_id),
   }
   return HttpResponse(template.render(context, request))
+
+def new(request):
+    template = loader.get_template("reviews/new.html")
+    album_id = request.GET.get('album_id', '')
+    album = Album.objects.get(id=album_id)
+    context = { 
+        "album": album,
+    }
+    return HttpResponse(template.render(context, request))
+
